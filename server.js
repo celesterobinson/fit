85776ad657c0c23cd0500b7bc348ca4e1d1a4a44
Luke Barrett <gotluke@gmail.com>
2018-02-14 14:24:23 -0700

    updated server

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const config = require("./config");
 
 const app = express();
 
-mongoose.connect("mongodb://localhost/group", (err) => {
+mongoose.connect(process.env.MONGODB_URI, (err) => {
     if (err) throw err;
     console.log("Connected to the database! :)");
 });
