689e9476eda3f34492120547a35d051429fed872
Celeste Robinson <celestekrobinson@gmail.com>
2018-02-15 10:16:56 -0700

    added index.js to redux folder, added store to src index.js

---
+++ client/src/App/redux/index.js
@@ -0,0 +1,21 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import exercises from "./exercises";
+
+const rootReducer = (
+    combineReducers({
+        exercises
+    })
+);
+
+let store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(thunk)
+);
+
+store.subscribe(() => {
+    console.log(store.getState());
+})
+
+export default store;
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {BrowserRouter as Router} from "react-router-dom";
-import {Provider} from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./App/redux/index";
 
 import App from "./App";
 
 ReactDOM.render(
-    <Provider>
+    <Provider store={store}>
         <Router>
             <App />
         </Router>
